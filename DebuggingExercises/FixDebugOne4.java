e8e3c26eba3c3e45bd3b76bb00f7716be2809e81
shoonleitun <45580219+shoonleitun@users.noreply.github.com>
2020-03-20 17:40:36 +0800

    Add FixDebug Files

---
+++ DebuggingExercises/FixDebugOne4.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class FixDebugOne4
+{
+    public static void main(String[] args)
+    {
+
+        JOptionPane.showMessageDialog(null, "First GUI program");
+    }
+}
